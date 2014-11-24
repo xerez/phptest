f0bb453bcfd4f2af558ea98f980cb4e26ca5168f
xerez <rurizo.apple@gmail.com>
2014-11-25 03:46:50 +0900

    １らいな

--- fizzbuzz.php
+++ fizzbuzz.php
@@ -1,3 +1,1 @@
-<?php
- for($i=1; $i<100; $i++){echo($i%15?$i%5?$i%3?$i:"fizz":"buzz":"fizzbuzz")."<br />";}
-?>
+<?php for($i=1; $i<100; $i++){echo($i%15?$i%5?$i%3?$i:"fizz":"buzz":"fizzbuzz")."<br />";}?>
